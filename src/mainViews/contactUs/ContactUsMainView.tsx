0e9d7de703d798fbfa715a77864bec15a8163c1f
Samiul Hasan <samiulxhasan650@gmail.com>
2023-01-11 12:10:40 +0600

    contactus code cleanup

--- src/mainViews/contactUs/ContactUsMainView.tsx
+++ src/mainViews/contactUs/ContactUsMainView.tsx
@@ -1,29 +1,15 @@
 import React, { useState } from "react";
+import Link from "next/link";
+import dayjs from "dayjs";
+import { Breadcrumb } from "react-bootstrap";
+import classNames from "classnames";
 import { Formik, Field, Form as FormikForm } from "formik";
-// import { Col, Row, Form, Container } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Form from "react-bootstrap/Form";
-import dayjs from "dayjs";
-
-import styles from "./contactUs.module.scss";
-import {
-  validateEmail,
-  validateRequired,
-  validURL,
-} from "../../shared/InternalService";
-import Card from "../../shared/components/card/Card";
-import Button from "../../shared/components/button/Button";
-import { contactUsEmailSend } from "../../shared/emailSend";
-import { triggerForm } from "../../services/internalServices";
-import axios from "axios";
-import Reaptcha from "reaptcha";
-import instance from "../../services/baseServices";
-import classNames from "classnames";
-import { Breadcrumb } from "react-bootstrap";
-import Link from "next/link";
 import { FacebookIcon, LinkedinIcon, TwitterIcon } from "react-share";
+import Reaptcha from "reaptcha";
 import {
   ArrowRight,
   Browser,
@@ -35,6 +21,18 @@ import {
   User,
   YoutubeLogo,
 } from "phosphor-react";
+
+import {
+  validateEmail,
+  validateRequired,
+  validURL,
+} from "../../shared/InternalService";
+import Card from "../../shared/components/card/Card";
+import Button from "../../shared/components/button/Button";
+import { contactUsEmailSend } from "../../shared/emailSend";
+import { triggerForm } from "../../services/internalServices";
+import instance from "../../services/baseServices";
+import styles from "./contactUs.module.scss";
 
 var utc = require("dayjs/plugin/utc");
 dayjs.extend(utc);
@@ -65,7 +63,6 @@ const ContactUsMainViewMainView = () => {
                 </Link>
                 <Breadcrumb.Item active>Contact Us</Breadcrumb.Item>
               </Breadcrumb>
-
               <h1 className="hero-title text-white text-center text-md-start mb-3">
                 Contact us
               </h1>
@@ -73,7 +70,6 @@ const ContactUsMainViewMainView = () => {
                 We intend to help you discover the best business contacts on the
                 market. Have questions? Don’t hesitate to contact us today!
               </p>
-
               <div className="d-flex align-items-start justify-content-center justify-content-md-start text-white mb-3">
                 <MapPin size={24} />{" "}
                 <h3 className="text-white ps-3">
@@ -186,7 +182,6 @@ const ContactUsMainViewMainView = () => {
                         <Col md={12}>
                           <Col md={12}>
                             <Form.Group className="mb-3">
-                              {/* <Form.Label htmlFor="name">Name</Form.Label> */}
                               <span className={styles.fieldWrapper}>
                                 <Field
                                   type="text"
@@ -208,7 +203,6 @@ const ContactUsMainViewMainView = () => {
                             </Form.Group>
                           </Col>
                           <Form.Group className="mb-3">
-                            {/* <Form.Label htmlFor="subject">Subject</Form.Label> */}
                             <span className={styles.fieldWrapper}>
                               <Field
                                 type="text"
@@ -230,7 +224,6 @@ const ContactUsMainViewMainView = () => {
                         </Col>
                         <Col md={12}>
                           <Form.Group className="mb-3">
-                            {/* <Form.Label htmlFor="email">Email</Form.Label> */}
                             <span className={styles.fieldWrapper}>
                               <Field
                                 id="email"
@@ -252,9 +245,6 @@ const ContactUsMainViewMainView = () => {
                         </Col>
                         <Col md={12}>
                           <Form.Group className="mb-3">
-                            {/* <Form.Label htmlFor="CompanyName">
-                              Company Name
-                            </Form.Label> */}
                             <span className={styles.fieldWrapper}>
                               <Field
                                 type="text"
@@ -276,9 +266,6 @@ const ContactUsMainViewMainView = () => {
                         </Col>
                         <Col md={12}>
                           <Form.Group className="mb-3">
-                            {/* <Form.Label htmlFor="CompanyName">
-                              Company Website
-                            </Form.Label> */}
                             <span className={styles.fieldWrapper}>
                               <Field
                                 type="text"
@@ -300,7 +287,6 @@ const ContactUsMainViewMainView = () => {
                         </Col>
                         <Col md={12}>
                           <Form.Group className="mb-3">
-                            {/* <Form.Label htmlFor="message">Message</Form.Label> */}
                             <Field
                               type="text"
                               id="message"
@@ -317,7 +303,6 @@ const ContactUsMainViewMainView = () => {
                           </Form.Group>
                         </Col>
                       </Row>
-
                       {values.name &&
                         values.companyName &&
                         values.companyWebsite &&
@@ -364,205 +349,6 @@ const ContactUsMainViewMainView = () => {
           </Row>
         </Container>
       </section>
-      {/* <section className={classNames(styles.wrapper, "sectiontopfix")}>
-        <Container className="py-4">
-          <Row className="justify-content-center">
-            <Col md={8}>
-              <h1 className={styles.header}>All Our databases are verified</h1>
-              <p>Ask us anything We are here to answer your questions !</p>
-              <Card title="Contact Us">
-                <Formik
-                  initialValues={{
-                    subject: "",
-                    name: "",
-                    email: "",
-                    companyName: "",
-                    message: "",
-                    companyWebsite: "",
-                  }}
-                  onSubmit={async (values, { resetForm }) => {
-                    try {
-                      setLoading(true);
-
-                      if (!capchaValue) {
-                        triggerForm({
-                          text: "Capcha field is empty",
-                          icon: "error",
-                          confirmButtonText: "OK",
-                        });
-
-                        setLoading(false);
-                        return;
-                      }
-
-                      await contactUsEmailSend(values);
-                      await instance.post("/update-contact-us", {
-                        ...values,
-                      });
-
-                      triggerForm({
-                        title: "",
-                        text: "Successfully Sent the Message",
-                        icon: "success",
-                        confirmButtonText: "OK",
-                      });
-
-                      resetForm();
-                      setLoading(false);
-                    } catch (error) {
-                      triggerForm({
-                        title: "",
-                        text: "Oops! Something went wrong!",
-                        icon: "error",
-                        confirmButtonText: "OK",
-                      });
-                      setLoading(false);
-                    }
-                  }}
-                >
-                  {({ errors, touched, isValidating, values }: any) => (
-                    <FormikForm>
-                      <Row>
-                        <Col md={12}>
-                          <Form.Group className="mb-3">
-                            <Form.Label htmlFor="subject">Subject</Form.Label>
-                            <Field
-                              type="text"
-                              id="subject"
-                              name="subject"
-                              placeholder="Subject"
-                              validate={validateRequired}
-                            />
-                            {errors.subject && (
-                              <Form.Text className="text-danger">
-                                {errors.subject}
-                              </Form.Text>
-                            )}
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                          <Form.Group className="mb-3">
-                            <Form.Label htmlFor="name">Name</Form.Label>
-                            <Field
-                              type="text"
-                              id="name"
-                              name="name"
-                              placeholder="Doe"
-                              validate={validateRequired}
-                            />
-                            {errors.name && (
-                              <Form.Text className="text-danger">
-                                {errors.name}
-                              </Form.Text>
-                            )}
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                          <Form.Group className="mb-3">
-                            <Form.Label htmlFor="email">Email</Form.Label>
-                            <Field
-                              id="email"
-                              name="email"
-                              placeholder="jane@acme.com"
-                              type="email"
-                              validate={validateEmail}
-                            />
-                            {errors.email && touched.email && (
-                              <Form.Text className="text-danger">
-                                {errors.email}
-                              </Form.Text>
-                            )}
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                          <Form.Group className="mb-3">
-                            <Form.Label htmlFor="CompanyName">
-                              Company Name
-                            </Form.Label>
-                            <Field
-                              type="text"
-                              id="companyName"
-                              name="companyName"
-                              placeholder="Company Name"
-                              validate={validateRequired}
-                            />
-                            {errors.companyName && (
-                              <Form.Text className="text-danger">
-                                {errors.companyName}
-                              </Form.Text>
-                            )}
-                          </Form.Group>
-                        </Col>
-                        <Col md={6}>
-                          <Form.Group className="mb-3">
-                            <Form.Label htmlFor="CompanyName">
-                              Company Website
-                            </Form.Label>
-                            <Field
-                              type="text"
-                              id="companyWebsite"
-                              name="companyWebsite"
-                              placeholder="Company Website"
-                              validate={validURL}
-                            />
-                            {errors.companyWebsite && (
-                              <Form.Text className="text-danger">
-                                {errors.companyWebsite}
-                              </Form.Text>
-                            )}
-                          </Form.Group>
-                        </Col>
-                        <Col md={12}>
-                          <Form.Group className="mb-3">
-                            <Form.Label htmlFor="message">Message</Form.Label>
-                            <Field
-                              type="text"
-                              id="message"
-                              name="message"
-                              placeholder="Your message"
-                              validate={validateRequired}
-                              as={CustomInputComponent}
-                            />
-                            {errors.message && (
-                              <Form.Text className="text-danger">
-                                {errors.message}
-                              </Form.Text>
-                            )}
-                          </Form.Group>
-                        </Col>
-                      </Row>
-
-                      {values.name &&
-                        values.companyName &&
-                        values.companyWebsite &&
-                        values.email &&
-                        values.message &&
-                        values.subject && (
-                          <Reaptcha
-                            sitekey={process.env.NEXT_PUBLIC_CAPTCHA_API_KEY}
-                            onVerify={onChange}
-                          />
-                        )}
-
-                      <Form.Group className="d-flex justify-content-center pt-3 mb-3">
-                        <Button
-                          size="large"
-                          block
-                          type="submit"
-                          loading={loading}
-                          disabled={loading}
-                        >
-                          Submit
-                        </Button>
-                      </Form.Group>
-                    </FormikForm>
-                  )}
-                </Formik>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </section> */}
     </>
   );
 };
