5a6d1f35313fba89d69a6f65ccd780818d48d833
Samiul Hasan <samiulxhasan650@gmail.com>
2023-03-18 15:48:30 +0600

    mega menu

--- src/menus/publicHeaderMenu/PublicHeaderMenu.tsx
+++ src/menus/publicHeaderMenu/PublicHeaderMenu.tsx
@@ -43,6 +43,15 @@ const NavigationLink: React.FC<IHeaderLinks> = ({
   const isDisabled =
     router.pathname === "/blogs/[blogDetail]" || router.pathname === "/blogs";
 
+  const icons = {
+    "/by-us-states": "/menu-icons/by-states.png",
+    "/job-titles": "/menu-icons/job-title.png",
+    "/industries": "/menu-icons/industries.png",
+    "/target": "/menu-icons/target.png",
+    "/international": "/menu-icons/international.png",
+    "/by-zip-code": "/menu-icons/zip-code.png",
+  };
+
   return (
     <li
       className={classNames(
@@ -87,7 +96,19 @@ const NavigationLink: React.FC<IHeaderLinks> = ({
                           </ul>
                         </div>
                       ) : (
-                        link.name
+                        <div>
+                          <span className={styles.menuIcon}>
+                            <Image
+                              // @ts-ignore
+                              src={icons[link.route]}
+                              width={35}
+                              height={35}
+                              objectFit="scale-down"
+                              alt={link.name}
+                            />
+                          </span>
+                          {link.name}
+                        </div>
                       )}
                     </li>
                   </Link>
