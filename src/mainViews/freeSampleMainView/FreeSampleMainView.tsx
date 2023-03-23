9365b0f34432b5def1d63541e1b5dbd644e7ab3a
Samiul Hasan <samiulxhasan650@gmail.com>
2023-03-23 12:19:11 +0600

    free sample alert

--- src/mainViews/freeSampleMainView/FreeSampleMainView.tsx
+++ src/mainViews/freeSampleMainView/FreeSampleMainView.tsx
@@ -6,6 +6,8 @@ import classNames from "classnames";
 import styles from "./styles.module.scss";
 import MainCategoryList from "./views/mainCategoryList/MainCategoryList";
 import instance from "../../services/baseServices";
+import Card from "../../shared/components/card/Card";
+import Image from "next/image";
 
 const FreeSampleMainView = () => {
   const router = useRouter();
@@ -26,8 +28,40 @@ const FreeSampleMainView = () => {
   return (
     <>
       <Row>
-        <Col xs={12}>
-          <h3>Free Sample</h3>
+        <Col xs={12} lg={7}>
+          <div className={styles.alertWrapper}>
+            <Row className="align-items-center">
+              <Col xs={12} lg={8}>
+                <span className={styles.title}>
+                  You have 1 more request left
+                </span>
+                <p>
+                  We currently offer {count} free product samples per month in
+                  any category you select.{" "}
+                  <span
+                    onClick={() => {
+                      router.push("/support");
+                    }}
+                    className={classNames(styles.contactus)}
+                  >
+                    Contact Us
+                  </span>{" "}
+                  for more sample list
+                </p>
+              </Col>
+              <Col xs={12} lg={4}>
+                <Image
+                  src="/free-sample-illustration.png"
+                  width={224}
+                  height={177}
+                  alt=""
+                />
+              </Col>
+            </Row>
+          </div>
+        </Col>
+        <Col xs={12} lg={5}>
+          test
         </Col>
         <Col xs={12} lg={10}>
           <Row className="align-items-center text-center text-md-start">
