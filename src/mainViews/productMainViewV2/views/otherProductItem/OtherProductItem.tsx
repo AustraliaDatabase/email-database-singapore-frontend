254d53f90d000955e6858f25f7b5b2bc5e26ab02
Samiul Hasan <samiulxhasan650@gmail.com>
2023-04-10 17:19:44 +0600

    other product feature section

--- src/mainViews/productMainViewV2/views/otherProductItem/OtherProductItem.tsx
+++ src/mainViews/productMainViewV2/views/otherProductItem/OtherProductItem.tsx
@@ -5,7 +5,7 @@ import styles from "./style.module.scss";
 const OtherProductItem = () => {
   return (
     <div className={styles.wrapper}>
-      <h3>Comprehensive Industry Coverage</h3>
+      <h3 className={styles.title}>Comprehensive Industry Coverage</h3>
       <p>
         The California Realtor Email List covers a wide range of industry
         professionals, from residential to commercial real estate, and
