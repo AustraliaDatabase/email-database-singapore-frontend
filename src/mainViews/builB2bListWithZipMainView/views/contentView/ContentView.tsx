e0ce0dbbe0745eb5b3b9c7f9de99976babe08bec
Asbar Ali <hanomain11@gmail.com>
2023-04-20 05:15:36 +0530

    fixes

--- src/mainViews/builB2bListWithZipMainView/views/contentView/ContentView.tsx
+++ src/mainViews/builB2bListWithZipMainView/views/contentView/ContentView.tsx
@@ -1,12 +1,105 @@
 import React from "react";
 
-interface IContentView {
-  content: any;
-}
+import styles from "./style.module.scss";
 
-const ContentView = (props: IContentView) => {
-  const { content } = props;
-  return <>{content}</>;
+const ContentView = () => {
+  return (
+    <div className={styles.wrapper}>
+      <section className="text-center text-lg-start py-0">
+        <h1 className="mt-0">
+          Target Your Ideal Audience with Mailing Lists by Zip Code
+        </h1>
+        <p>
+          If you&#39;re looking to promote your business to a specific
+          demographic, using zip code mailing list can be a smart strategy.  By
+          targeting people who share the same zip code, you can reach prospects
+          who have things in common, such as their interests, needs, and
+          purchasing habits. <br /><br /> Plus, if you&#39;re running a local business, you
+          can use zip code mailing lists to focus your marketing efforts on
+          nearby customers.
+          At EmailDatas, we offer accurate and complete email lists by zip code
+          that can help you maximize your resources, save time, and increase
+          your ROI. <br /><br />Whether you need business or consumer data, our mailing
+          lists are carefully verified to ensure that you get the most relevant
+          and up-to-date leads. With our targeted email lists by zip code, you
+          can easily customize your message and connect with the right people.
+        </p>
+      </section>
+      <section className="text-center text-lg-start  pb-0">
+        <h2>
+          Boost Your Marketing Campaigns with Cost-Effective Email List by Zip
+          Code
+        </h2>
+        <p>
+          Are you tired of spending a fortune on advertising without seeing
+          results? With our email lists by zip code, you can reach your target
+          audience in a cost-effective way. Instead of paying for expensive TV
+          ads, billboards, or radio spots, you can invest in targeted email
+          campaigns that deliver a high ROI.
+        </p>
+        <p>
+          Our email lists by zip code are not only affordable, but also
+          well-organized and easy to use. You can select the zip codes that are
+          most relevant to your business, and start sending out personalized
+          emails to potential customers.<br /><br /> Whether you&#39;re promoting a new
+          product, announcing a special deal, or simply building your brand, our
+          email lists can help you get the word out and attract more customers.
+        </p>
+      </section>
+
+      <section className="text-center text-lg-start  pb-0">
+        <h2>Get Reliable and Verified Leads for Your B2B Marketing</h2>
+        <p>
+          If you&#39;re running a B2B business, you know how important it is to
+          have a reliable source of leads. With our business Email lists by zip
+          code, you can access a wealth of data about companies in your area.
+          Our lists are carefully verified to ensure that you get accurate and
+          up-to-date information, including the company name, address, phone
+          number, and email address.
+        </p>
+
+        <p>
+          Whether you&#39;re targeting small businesses or large corporations,
+          our mailing lists by zip code can help you connect with
+          decision-makers and influencers. You can use our data to run email
+          campaigns, cold calls, or direct mail campaigns, and increase your
+          chances of closing deals. With our premium email lists by zip code,
+          you can stay ahead of your competition and grow your business.
+        </p>
+      </section>
+
+      <section className="text-center text-lg-start  pb-0">
+        <h2>Master Local Marketing with Free Email Lists by Zip Code</h2>
+        <p>
+          At EmailDatas, we&#39;re committed to helping you succeed in your
+          marketing efforts. That&#39;s why we offer free email lists by zip
+          code to all our clients. Whether you&#39;re a small business owner or
+          a marketing professional, you can benefit from our high-quality data
+          and expert support.
+        </p>
+        <p>
+          With our free email lists by zip code, you can test your campaigns,
+          try new strategies, and see how your audience responds. You can use
+          our data to target specific niches, neighborhoods, or regions, and
+          expand your reach over time. With our help, mastering local marketing
+          has never been easier.
+        </p>
+      </section>
+      <section className="text-center text-lg-start  pb-0">
+        <h2>Conclusion</h2>
+        <p>
+          No matter what type of business you&#39;re running, having access to
+          reliable and targeted mailing lists can make a big difference in your
+          marketing efforts. By using zip code mailing lists, you can reach
+          prospects who are most likely to be interested in your products or
+          services. <br /><br /> At EmailDatas, we offer accurate, complete, and verified
+          email lists by zip code that can help you boost your campaigns, save
+          time, and increase your revenues. Contact us today to learn more about
+          our services and start growing your business!
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default ContentView;
