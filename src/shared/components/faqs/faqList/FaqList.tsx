6cc94f8e20d5d02c0d8d8e508f22afc01ef11769
Samiul Hasan <samiulxhasan650@gmail.com>
2023-04-20 17:28:23 +0600

    faq accordion

--- src/shared/components/faqs/faqList/FaqList.tsx
+++ src/shared/components/faqs/faqList/FaqList.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Target } from "phosphor-react";
+import { CaretDown, Target } from "phosphor-react";
 
 import { FaqItems } from "./interface";
 import styles from "./styles.module.scss";
+import { Accordion } from "react-bootstrap";
 
 interface IFaqList {
   faqItem: FaqItems;
@@ -16,10 +17,22 @@ const FaqList = (props: IFaqList) => {
         <div className={styles.icon}>
           <Target size={32} />
         </div>
-        <div>
-        <h3 className={styles.title}>{faqItem.title}</h3>
-          <div>{faqItem.element}</div>
-        </div>
+        <Accordion
+          defaultActiveKey="0"
+          className="beneifit-accordion faq-accordion"
+        >
+          <Accordion.Item eventKey="" className="mb-0">
+            <Accordion.Header className="d-flex align-items-start">
+              <h3 className={styles.title}>{faqItem.title}</h3>
+              <div>
+                <CaretDown className="icon me-3" size={24} weight="bold" />
+              </div>
+            </Accordion.Header>
+            <Accordion.Body>
+              <div>{faqItem.element}</div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </div>
     </>
   );
