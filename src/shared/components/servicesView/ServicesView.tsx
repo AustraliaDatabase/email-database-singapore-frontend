217ad0062768516d4b86ec93c87a68c596f5da5e
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2023-06-13 13:02:42 +0600

    changes

--- src/shared/components/servicesView/ServicesView.tsx
+++ src/shared/components/servicesView/ServicesView.tsx
@@ -5,38 +5,48 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 import { BUTTON_VARIANT_ENUM } from '../../enums';
 import Button from '../button/Button';
-import styles from "./styles.module.scss"
+import styles from './styles.module.scss';
 
 interface IservicesList {
     image: string;
     title: string;
     description: string;
     buttonLabel: string;
-    buttonLink: string
+    buttonLink: string;
 }
+
 interface IServiceView {
     title: string;
     description: string;
     services: IservicesList[];
 }
 
-const ServicesView = (props: IServiceView) => {
+const ServicesView: React.FC<IServiceView> = (props) => {
     const { title, description, services } = props;
     const router = useRouter();
 
     const [expandedCardIndexes, setExpandedCardIndexes] = useState<number[]>([]);
-    const maxDescriptionLength = 250;
+    const maxDescriptionLength = 275;
 
     const toggleCardExpand = (index: number) => {
-        if (expandedCardIndexes.includes(index)) {
-            setExpandedCardIndexes(expandedCardIndexes.filter(i => i !== index));
-        } else {
-            setExpandedCardIndexes([...expandedCardIndexes, index]);
-        }
+        setExpandedCardIndexes((prevExpandedCardIndexes) => {
+            if (prevExpandedCardIndexes.includes(index)) {
+                return prevExpandedCardIndexes.filter((i) => i !== index);
+            } else {
+                return [...prevExpandedCardIndexes, index];
+            }
+        });
     };
 
     const isCardExpanded = (index: number) => {
         return expandedCardIndexes.includes(index);
+    };
+
+    const truncateText = (text: string, maxLength: number): string => {
+        if (text.length > maxLength) {
+            return text.slice(0, maxLength) + '...';
+        }
+        return text;
     };
 
     return (
@@ -48,8 +58,9 @@ const ServicesView = (props: IServiceView) => {
             <Row className={styles.cardsRow}>
                 {services.map((service: IservicesList, index: number) => {
                     const isExpanded = isCardExpanded(index);
-                    const truncatedDescription = service.description.slice(0, maxDescriptionLength);
+                    const truncatedDescription = truncateText(service.description, maxDescriptionLength);
                     const isDescriptionTruncated = service.description.length > maxDescriptionLength;
+
                     return (
                         <Col xs={12} lg={6} key={index}>
                             <div className={styles.serviceCard}>
@@ -61,21 +72,21 @@ const ServicesView = (props: IServiceView) => {
                                     {isExpanded ? service.description : truncatedDescription}
                                     {isDescriptionTruncated && (
                                         <span className={styles.seeMore} onClick={() => toggleCardExpand(index)}>
-                                            {isExpanded ? 'See less' : "See More"}
+                                            {isExpanded ? 'See less' : 'See more'}
                                         </span>
                                     )}
                                 </div>
                                 <div className='text-center mt-4'>
-                                    <Button className={styles.button} variant={BUTTON_VARIANT_ENUM.Primary} onClick={() => router.push(service.buttonLink)} >
+                                    <Button className={styles.button} variant={BUTTON_VARIANT_ENUM.Primary} onClick={() => router.push(service.buttonLink)}>
                                         {service.buttonLabel}
                                     </Button>
                                 </div>
                             </div>
                         </Col>
-                    )
+                    );
                 })}
             </Row>
-        </Container >
+        </Container>
     );
 };
 
