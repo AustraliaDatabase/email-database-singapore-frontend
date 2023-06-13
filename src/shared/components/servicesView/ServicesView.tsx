a033bae9c7e54e218072d8ee5fdfe5f97cc26847
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2023-06-13 11:17:41 +0600

    styles

--- src/shared/components/servicesView/ServicesView.tsx
+++ src/shared/components/servicesView/ServicesView.tsx
@@ -5,7 +5,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import Button from '../button/Button';
 import styles from "./styles.module.scss"
 import Image from 'next/image';
-
+import { BUTTON_VARIANT_ENUM } from '../../enums';
 interface IservicesList {
     image: string;
     title: string;
@@ -13,12 +13,10 @@ interface IservicesList {
     buttonLabel: string;
     buttonLink: string
 }
-
 interface IServiceView {
     title: string;
     description: string;
     services: IservicesList[];
-
 }
 
 const ServicesView = (props: IServiceView) => {
@@ -27,21 +25,26 @@ const ServicesView = (props: IServiceView) => {
     return (
         <Container>
             <div className={styles.title} dangerouslySetInnerHTML={{ __html: title }} />
-            <div className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
-            <Row>
+            <Col xs={12} md={10} lg={8} className='mx-auto text-center mt-4'>
+                <div className={styles.description} dangerouslySetInnerHTML={{ __html: description }} />
+            </Col>
+            <Row className={styles.cardsRow}>
                 {services.map((service, index) => {
                     return (
                         <Col xs={12} lg={6} key={index}>
                             <div className={styles.serviceCard}>
                                 <div className={styles.iconWrapper}>
-                                    <Image src={service.image} width={88} height={88} objectFit='scale-down' alt={`${service.buttonLabel} icon`} />
+                                    <Image src={service.image} width={66} height={66} objectFit='scale-down' alt={`${service.buttonLabel} icon`} />
                                 </div>
                                 <div className={styles.cardTitle} dangerouslySetInnerHTML={{ __html: service.title }} />
                                 <div className={styles.cardDescription} dangerouslySetInnerHTML={{ __html: service.description }} />
-                                <Button onClick={() => router.push(service.buttonLink)} >{service.buttonLabel}</Button>
+                                <div className='text-center mt-4'>
+                                    <Button className={styles.button} variant={BUTTON_VARIANT_ENUM.Primary} onClick={() => router.push(service.buttonLink)} >
+                                        {service.buttonLabel}
+                                    </Button>
+                                </div>
                             </div>
                         </Col>
-
                     )
                 })}
             </Row>
