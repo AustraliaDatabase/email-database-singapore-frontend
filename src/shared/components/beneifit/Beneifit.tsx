fc6c9259478b7c8b8e7122f53317779ed6730c7d
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2023-06-13 13:20:49 +0600

    collapsible beneifits only product page

--- src/shared/components/beneifit/Beneifit.tsx
+++ src/shared/components/beneifit/Beneifit.tsx
@@ -9,6 +9,7 @@ import { DATA_TYPE_TO_TITLE_FOR_CONTENT } from "../../constants";
 import { numberWithCommas } from "../../InternalService";
 import { Col } from "react-bootstrap";
 import Card from "../card/Card";
+import { useRouter } from "next/router";
 
 interface IBeneifitView {
   beneifitInfo: IBeneifits;
@@ -18,20 +19,18 @@ interface IBeneifitView {
 const dynamicBenefitList = (currentObject: IMainProductInfo) => [
   {
     title: `Email Marketing`,
-    description: `Are you looking to enhance your brand marketing strategy? Look no further than email marketing, which drives up to 90% of sales for many businesses. With our ${
-      currentObject?.name
-    } email marketing lists, boasting a count of ${numberWithCommas(
-      currentObject?.stats?.emailAddress?.toString() || ""
-    )}, we offer a deliverability rate of 95%, ensuring a successful campaign. `,
+    description: `Are you looking to enhance your brand marketing strategy? Look no further than email marketing, which drives up to 90% of sales for many businesses. With our ${currentObject?.name
+      } email marketing lists, boasting a count of ${numberWithCommas(
+        currentObject?.stats?.emailAddress?.toString() || ""
+      )}, we offer a deliverability rate of 95%, ensuring a successful campaign. `,
   },
   {
     title: `Telemarketing`,
     description: `
     We offer high-quality telemarketing lists, including ${numberWithCommas(
       currentObject?.stats?.phoneNumber?.toString() || "0"
-    )} mobile numbers for potential customers, all at an affordable rate for businesses in the USA. Our ${
-      currentObject?.name
-    } lists provide a straightforward and effective approach, allowing you to directly contact prospects for a fast response. 
+    )} mobile numbers for potential customers, all at an affordable rate for businesses in the USA. Our ${currentObject?.name
+      } lists provide a straightforward and effective approach, allowing you to directly contact prospects for a fast response. 
     `,
   },
   {
@@ -48,18 +47,18 @@ const dynamicBenefitList = (currentObject: IMainProductInfo) => [
   },
   {
     title: `Direct Mailing Lists`,
-    description: `If you're looking to specifically target ${
-      currentObject?.name
-    }, our Direct Mailing Lists can help. With a count of ${numberWithCommas(
-      currentObject?.stats?.address?.toString() || "0"
-    )}  addresses for ${
-      currentObject?.name
-    }, you can conveniently reach top-level executives and increase your chances of a successful marketing campaign.`,
+    description: `If you're looking to specifically target ${currentObject?.name
+      }, our Direct Mailing Lists can help. With a count of ${numberWithCommas(
+        currentObject?.stats?.address?.toString() || "0"
+      )}  addresses for ${currentObject?.name
+      }, you can conveniently reach top-level executives and increase your chances of a successful marketing campaign.`,
   },
 ];
 
 const BeneifitView = (props: IBeneifitView) => {
   const { beneifitInfo, currentObject } = props;
+  const router = useRouter();
+  const isProductPage = router.pathname == "/[productId]";
 
   const benefitList = currentObject
     ? dynamicBenefitList(currentObject)
@@ -77,28 +76,31 @@ const BeneifitView = (props: IBeneifitView) => {
         <div className={styles.beneifitDescription}>
           {currentObject
             ? `Discover the full potential of our ${currentObject?.name}
-          ${
-            DATA_TYPE_TO_TITLE_FOR_CONTENT[currentObject?.type]
-          } List! Our comprehensive
+          ${DATA_TYPE_TO_TITLE_FOR_CONTENT[currentObject?.type]
+            } List! Our comprehensive
           directory includes verified email IDs, phone numbers, company names,
           office addresses, and other key details.`
             : beneifitInfo?.description}
         </div>
       </div>
       <Row>
-        {benefitList.map((benefit, index) => {
-          return (
-            <Col key={index} xs={12} lg={6} className="mt-4">
-              <Card>
-                <h3 className={styles.title}>{benefit.title}</h3>
-                <div
-                  className={styles.description}
-                  dangerouslySetInnerHTML={{ __html: benefit.description }}
-                />
-              </Card>
-            </Col>
-          );
-        })}
+        {isProductPage ? <BeneifitsCollapsible BeneifitList={benefitList} /> :
+          <>
+            {benefitList.map((benefit, index) => {
+              return (
+                <Col key={index} xs={12} lg={6} className="mt-4">
+                  <Card>
+                    <h3 className={styles.title}>{benefit.title}</h3>
+                    <div
+                      className={styles.description}
+                      dangerouslySetInnerHTML={{ __html: benefit.description }}
+                    />
+                  </Card>
+                </Col>
+              );
+            })}
+          </>
+        }
       </Row>
     </Container>
   );
