382fe05e0bacc8d25122dff2c902680ac5044c0d
Asbar Ali <hanomain11@gmail.com>
2023-06-30 19:57:52 +0530

    update faq

--- src/mainViews/mainProduct/views/faqs/Faqs.tsx
+++ src/mainViews/mainProduct/views/faqs/Faqs.tsx
@@ -6,11 +6,6 @@ import Col from "react-bootstrap/Col";
 import { ICollapsibleItem } from "../../../../shared/components/collapsibleList/interface";
 import Faqs from "../../../../shared/components/faqs/Faqs";
 import { IMainProductInfo } from "../../../../shared/interface";
-import {
-  DATA_TYPE_TO_TITLE,
-  DATA_TYPE_TO_TITLE_FOR_CONTENT,
-} from "../../../../shared/constants";
-import { numberWithCommas } from "../../../../shared/InternalService";
 
 interface IFaqsView {
   faqsList: ICollapsibleItem[];
@@ -18,75 +13,14 @@ interface IFaqsView {
   currentObject?: IMainProductInfo;
 }
 
-const updateDynamicFaqsSeed = (currentObject: IMainProductInfo) => {
-  const type = currentObject?.type
-    ? DATA_TYPE_TO_TITLE[currentObject?.type]
-    : "";
-  const typeContent = currentObject?.type
-    ? DATA_TYPE_TO_TITLE_FOR_CONTENT[currentObject?.type]
-    : "";
-  const name = currentObject?.name;
-  const count = currentObject?.stats?.emailAddress;
-
-  return [
-    {
-      title: `Are there any duplicate contacts in your ${name} ${typeContent} List?`,
-      element: `No, all the contacts in our list are unique, so you won't receive any duplicates.`,
-    },
-    {
-      title: `How Often do you update this ${name} database?`,
-      element: `We update our list every three months to ensure that you receive only fresh and valid contacts. As of today, our ${name} ${typeContent} List contains ${numberWithCommas(
-        count?.toString() || "0"
-      )} contacts.
-        `,
-    },
-    {
-      title: `What payment method do you accept for the ${name} List?`,
-      element: (
-        <p>
-          We accept major credit card payments such as VISA, MasterCard,
-          American Express, and Discover via PayPal payment gateway. PayPal is
-          100% secured payment method for buyers. Therefore, your payment is
-          secured. <br /> <br /> We also accept Bitcoin payments which will be
-          processed via Coinpayment.net and is 100% secured.
-        </p>
-      ),
-    },
-    {
-      title: `How do you deliver the ${name} List?`,
-      element: `You can download the ${name} List directly from the dashboard of our website. You can download it for 1 year and will get the latest updates every three months.`,
-    },
-    {
-      title: `Can I export the ${name} List to CRM software?`,
-      element: `Yes, our files are in CSV format, therefore you can easily feed them to any CRM platform.`,
-    },
-    {
-      title: `Can I purchase a specific ${type} other than the ${name} List?`,
-      element: `Yes, you can purchase a ${type} based on your specific requirements. We have not listed all of our contacts yet, so please feel free to contact us for custom requirements.`,
-    },
-    {
-      title: `Where do these contacts in the ${name} List come from?`,
-      element: `These data were collected from publicly available sources, LinkedIn, Yellow pages, Yelp, associations, MLS, licensing boards, government databases, and more.`,
-    },
-    {
-      title: `Will I get unlimited access and usage of the ${name} List?`,
-      element: `Yes, we will sell actual contact files to you. You will get complete access to this file. You will get 100% ownership of the files.`,
-    },
-  ];
-};
-
 const FaqsView = (props: IFaqsView) => {
   const { faqsList, title, currentObject } = props;
-
-  const updatedFaqList = currentObject
-    ? updateDynamicFaqsSeed(currentObject)
-    : faqsList;
 
   return (
     <Container>
       <Row>
         <Col>
-          <Faqs faqsList={updatedFaqList} title={title} description="" />
+          <Faqs faqsList={faqsList} title={title} description="" />
         </Col>
       </Row>
     </Container>
--- src/shared/components/faqs/faqsSeeds.tsx
+++ src/shared/components/faqs/faqsSeeds.tsx
@@ -1,42 +1,52 @@
 export const FaqsSeed = [
   {
-    title: "Do you sell Unique B2B Email Contacts only?",
-    element: "Yes! There will be zero duplicate contacts.",
+    title: "What is the source of your Australia Database?",
+    element:
+      "Our Australia Database is sourced from various trusted and reliable channels, including opt-in forms, business directories, and reputable data providers.",
   },
   {
-    title: "How Often do you update this B2B Email database?",
-    element: "We update all contacts every three months. You will get only fresh and valid contacts.",
+    title: "What type of information is included in the Australia Database?",
+    element:
+      "Our Australia Database contains email addresses of individuals and businesses located in Australia. It may also include additional information such as names, phone numbers, company details, and more (depending on the specific package you choose).",
   },
   {
-    title: "Do you provide 1 year free updates for the B2B Email list?",
-    element: "Yes, we provide one year free updates since your purchase date.",
+    title: "Is your Australia Database regularly updated?",
+    element:
+      "Yes, we strive to keep our Australia Database up-to-date by implementing regular verification processes. We remove outdated or inactive email addresses to ensure that you receive accurate and reliable data.",
   },
   {
-    title: "What payment method do you accept for the Email List?",
-    element: "We accept major credit card payments such as VISA, MasterCard, American Express, and Discover via PayPal payment gateway. PayPal is 100% secured payment method for buyers. Therefore, your payment is secured. We also accept Bitcoin payments which will be processed via Coinpayment.net and is 100% secured.",
+    title: "How can I purchase the Australia Database?",
+    element:
+      "To purchase our Australia Database, simply visit our website and browse through the available packages. Select the desired package, make the payment, and you'll receive instant access to download the database.",
   },
   {
-    title: "How do you deliver the List?",
-    element: "You can download the List directly from the dashboard of our website. You can download it for 1 year and will get the latest updates every three months.",
+    title: "Can I customize the Australia Database based on my specific needs?",
+    element:
+      "Yes, we offer customization options for our Australia Database. Please contact our customer support team with your requirements, and we will assist you in creating a tailored database package.",
   },
   {
-    title: "Can I export the List to CRM software?",
-    element: "Yes, our files are in CSV format, therefore you can easily feed them to any CRM platform.",
+    title: "What format is the Australia Database available in?",
+    element:
+      "The Australia Database is typically provided in a common format such as CSV (Comma-Separated Values) or Excel. This allows for easy importing and compatibility with various software and platforms.",
   },
   {
-    title: "Can I purchase a single state in USA, such as the Alabama List?",
-    element: "Yes, you can purchase a business database by states.",
+    title: "Is the Australia Database compliant with privacy laws?",
+    element:
+      "We prioritize compliance with privacy laws and regulations. Our data collection and storage processes adhere to applicable laws, including the General Data Protection Regulation (GDPR) and the Australian Privacy Principles (APPs).",
   },
   {
-    title: "Can I purchase a specific job title or industry or targeted list in the Complete B2B List?",
-    element: "Yes, you can purchase a specific targeted list as per your requirements in our List.",
+    title: "Can I use the Australia Database for marketing purposes?",
+    element:
+      "Yes, you can use the Australia Database for marketing purposes, such as sending targeted email campaigns or reaching out to potential customers. However, it is important to ensure that your marketing practices comply with relevant laws and regulations.",
   },
   {
-    title: "Where do these contacts in your List come from?",
-    element: "These data were collected from publicly available sources, LinkedIn, Yellow pages, Yelp, associations, MLS, licensing boards, government databases, and more.",
+    title: "Do you offer refunds for the Australia Database?",
+    element:
+      "As the Australia Database is a digital product, we do not offer refunds once the database has been downloaded. We encourage you to thoroughly review the details and sample data before making a purchase.",
   },
   {
-    title: "Will I get unlimited access and usage of the List?",
-    element: "Yes, we will sell actual contact files to you. You will get complete access to this file. You will get 100% ownership of the files.",
+    title: "How can I contact your customer support team?",
+    element:
+      "You can contact our customer support team by visiting the 'Contact Us' section on our website. We are available to assist you with any queries or concerns you may have regarding the Australia Database.",
   },
 ];
